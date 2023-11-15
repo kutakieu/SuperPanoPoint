8a7b35a7cced1b8274d52afba35de2c54ce1a5cb
kutakieu <kutakieu22@gmail.com>
2023-11-15 10:24:56 +0900

    add desctiptor loss

--- superpanopoint/lossfn_optimizer.py
+++ superpanopoint/lossfn_optimizer.py
@@ -1,7 +1,9 @@
 from typing import Literal
 
-import torch.nn as nn
+import torch
 import torch.optim as optim
+from einops import rearrange
+from torch import Tensor, nn
 
 LossFunctionType = Literal["cross_entropy", "binary_cross_entropy"]
 OptimizerType = Literal["sgd", "adam"]
@@ -14,10 +16,20 @@ def loss_function_factory(loss_function_name: LossFunctionType):
         return nn.BCELoss()
     raise NotImplementedError
 
-
 def optimizer_factory(optimizer_name: OptimizerType, learning_rate: float, net):
     if optimizer_name == "sgd":
         return optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
     elif optimizer_name == "adam":
         return optim.Adam(net.parameters(), lr=learning_rate)
     raise NotImplementedError
+
+def pointness_loss(pred_points: Tensor, gt_points: Tensor):
+    return nn.BCELoss()(pred_points, gt_points)
+
+def descriptor_loss(desc: Tensor, warped_desc: Tensor, points: Tensor, warped_points: Tensor, correspondence_mask: Tensor, _lambda: float=50.0, pos_margin = 0.5, neg_margin = 0.5):
+    desc = rearrange(desc, "b h w c -> b (h w) c")
+    warped_desc = rearrange(warped_desc, "b h w c -> b (h w) c")
+    ele_wise_dot = torch.einsum("bnc,bmc->bnm", desc, warped_desc)
+    pos_corres_loss = _lambda * correspondence_mask * torch.maximum(0, 0.5 - ele_wise_dot)
+    neg_corres_loss = (1 - correspondence_mask) * torch.maximum(0, ele_wise_dot - 0.5)
+    return pos_corres_loss + neg_corres_loss
