1e8974d092882a11f119967e460a3f8d5bea6efc
kutakieu <kutakieu22@gmail.com>
2023-11-20 10:30:53 +0900

    wip

--- superpanopoint/datasets/data_synth/shapes.py
+++ superpanopoint/datasets/data_synth/shapes.py
@@ -200,9 +200,8 @@ class Polygon(Shape):
         if self.is_overlap(self.drawing_coords(img), img, bg_img):
             return False
         
-        col = get_random_color(int(np.mean(img)))
         corners = np.array([p.as_xy() for p in self.points], dtype=int)
-        cv2.fillPoly(img, [corners], col)
+        cv2.fillPoly(img, [corners], self.color)
         return True
 
     def drawing_coords(self, img: np.ndarray):
@@ -213,10 +212,111 @@ class Polygon(Shape):
             
 
 class Star(Shape):
-    pass
+    img_width: int
+    img_height: int
+    thickness: Optional[int] = None
+    color: Optional[Tuple[int]] = None
+    points: List[Point2D] = field(default_factory=list)
+    num_branches: int = None
+
+    def __post_init__(self):
+        if self.color is None:
+            self.color = get_random_color(0)
+        if self.points is None or len(self.points) < 3:
+            self.points = self.__create_points()
+        if self.thickness is None:
+            min_dim = min(self.img_height, self.img_width)
+            self.thickness = random_state.randint(min_dim * 0.01, min_dim * 0.02)
+        if self.num_branches is None:
+            self.num_branches = random_state.randint(3, 6)
+
+    def __create_points(self) -> List[Point2D]:
+        min_dim = min(self.img_height, self.img_width)
+        rad = max(random_state.rand() * min_dim / 2, min_dim / 5)
+        x = random_state.randint(rad, self.img_width - rad)  # select the center of a circle
+        y = random_state.randint(rad, self.img_height - rad)
+        # Sample num_branches points inside the circle
+        slices = np.linspace(0, 2 * math.pi, self.num_branches + 1)
+        angles = [slices[i] + random_state.rand() * (slices[i+1] - slices[i])
+                for i in range(self.num_branches)]
+        points = np.array([[int(x + max(random_state.rand(), 0.3) * rad * math.cos(a)),
+                            int(y + max(random_state.rand(), 0.3) * rad * math.sin(a))]
+                        for a in angles])
+        points = np.concatenate(([[x, y]], points), axis=0)
+        return [Point2D(p[0], p[1]) for p in points]
+
+    def draw(self, img: np.ndarray, bg_img: np.ndarray):
+        if self.is_overlap(self.drawing_coords(img), img, bg_img):
+            return False
+
+        for i in range(1, self.num_branches + 1):
+            cv2.line(img, 
+                     (self.points[0].x, self.points[0].y), (self.points[i].x, self.points[i].y),
+                     self.color, self.thickness
+                     )
+        return True
+
+    def drawing_coords(self, img: np.ndarray):
+        tmp_img = np.zeros(img.shape[:2])
+        for i in range(1, self.num_branches + 1):
+            cv2.line(img, 
+                     (self.points[0].x, self.points[0].y), (self.points[i].x, self.points[i].y),
+                     self.color, self.thickness
+                     )
+        return np.where(tmp_img > 0)
 
 class Checkerboard(Shape):
-    pass
+    img_width: int
+    img_height: int
+    background_color: Optional[Tuple[int]] = None
+    points: List[Point2D] = field(default_factory=list)
+    rows: Optional[int] = None
+    cols: Optional[int] = None
+
+    def __post_init__(self):
+        if self.background_color is None:
+            self.color = get_random_color(0)
+        if self.points is None or len(self.points) < 3:
+            self.points = self.__create_points()
+        if self.rows is None:
+            self.rows = random_state.randint(3, 7)
+        if self.cols is None:
+            self.cols = random_state.randint(3, 7)
+            
+    def __create_points(self) -> List[Point2D]:
+        min_dim = min(self.img_height, self.img_width)
+        rad = max(random_state.rand() * min_dim / 2, min_dim / 5)
+        x = random_state.randint(rad, self.img_width - rad)  # select the center of a circle
+        y = random_state.randint(rad, self.img_height - rad)
+        # Sample num_branches points inside the circle
+        slices = np.linspace(0, 2 * math.pi, self.num_branches + 1)
+        angles = [slices[i] + random_state.rand() * (slices[i+1] - slices[i])
+                for i in range(self.num_branches)]
+        points = np.array([[int(x + max(random_state.rand(), 0.3) * rad * math.cos(a)),
+                            int(y + max(random_state.rand(), 0.3) * rad * math.sin(a))]
+                        for a in angles])
+        points = np.concatenate(([[x, y]], points), axis=0)
+        return [Point2D(p[0], p[1]) for p in points]
+
+    def draw(self, img: np.ndarray, bg_img: np.ndarray):
+        if self.is_overlap(self.drawing_coords(img), img, bg_img):
+            return False
+
+        for i in range(1, self.num_branches + 1):
+            cv2.line(img, 
+                     (self.points[0].x, self.points[0].y), (self.points[i].x, self.points[i].y),
+                     self.color, self.thickness
+                     )
+        return True
+
+    def drawing_coords(self, img: np.ndarray):
+        tmp_img = np.zeros(img.shape[:2])
+        for i in range(1, self.num_branches + 1):
+            cv2.line(img, 
+                     (self.points[0].x, self.points[0].y), (self.points[i].x, self.points[i].y),
+                     self.color, self.thickness
+                     )
+        return np.where(tmp_img > 0)
 
 class Stripe(Shape):
     pass
