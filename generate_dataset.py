cb829677503be8a78028ea32f29b137a054b4e9c
kutakieu <kutakieu22@gmail.com>
2023-11-25 17:45:55 +0900

    fix generator

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from random import randint
+from random import choice, randint
 
 from superpanopoint.datasets.data_synth import SynthData
 from superpanopoint.datasets.data_synth.shapes import (Checkerboard, Cube,
                                                        Line, Polygon, Star,
                                                        Stripe)
 
-NUM_SAMPLES = 10
+NUM_SAMPLES = 1024
 W, H = 512, 512
 
 NUM_MIN_LINES = 3
@@ -17,24 +17,17 @@ NUM_MIN_STARS = 1
 NUM_MAX_STARS = 3
 
 def main():
-    out_dir = Path("data/synthetic/persepective")
+    out_dir = Path("data/synthetic/perspective")
     out_dir.mkdir(parents=True, exist_ok=True)
     for i in range(NUM_SAMPLES):
         synth_data = SynthData(W, H)
+        if randint(0, 1) == 0:
+            adding_shape = choice([Cube, Checkerboard, Stripe])
+            synth_data.add_shapes(adding_shape, 1)
+
         synth_data.add_shapes(Line, randint(NUM_MIN_LINES, NUM_MAX_LINES))
         synth_data.add_shapes(Polygon, randint(NUM_MIN_POLYGONS, NUM_MAX_POLYGONS))
         synth_data.add_shapes(Star, randint(NUM_MIN_STARS, NUM_MAX_STARS))
-        synth_data.export(out_dir, str(i))
-        synth_data.export_img(out_dir / "with_points" / f"{str(i)}.png", with_points=True)
-
-    for i in range(NUM_SAMPLES, NUM_SAMPLES*2):
-        synth_data = SynthData(W, H)
-        if i % 3 == 0:
-            synth_data.add_shapes(Cube, 1)
-        elif i % 3 == 1:
-            synth_data.add_shapes(Checkerboard, 1)
-        else:
-            synth_data.add_shapes(Stripe, 1)
         synth_data.export(out_dir, str(i))
         synth_data.export_img(out_dir / "with_points" / f"{str(i)}.png", with_points=True)
 
