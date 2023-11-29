bd643d457645e7f6ae76eb120a18670acf7e3032
kutakieu <kutakieu22@gmail.com>
2023-11-29 22:31:41 +0900

    fix homographic training

--- superpanopoint/datasets/homographic.py
+++ superpanopoint/datasets/homographic.py
@@ -3,7 +3,10 @@ from typing import List
 import numpy as np
 import torch
 from einops import rearrange
+from PIL import Image
 from torchvision.transforms.v2 import Compose, Normalize, ToTensor
+
+from superpanopoint.models.detector import Predictor
 
 from . import BaseDataset, DataSample
 from .data_synth.homographies import (TransformHomography,
@@ -11,15 +14,16 @@ from .data_synth.homographies import (TransformHomography,
 
 
 class HomographicDataset(BaseDataset):
-    def __init__(self, data_samples: List[DataSample], **kwargs):
+    def __init__(self, data_samples: List[DataSample], point_detector: Predictor, crop_size: int=360, flip: bool=True, **kwargs):
         super().__init__(**kwargs)
         self.data_samples = data_samples
+        self.point_detector = point_detector
+        self.crop_size = crop_size
+        self.flip = flip
         self.img_transform = Compose([
             ToTensor(),
             Normalize(mean=[0.449], std=[0.226])
         ])
-        common_transform_list = []
-        self.common_transform = Compose(common_transform_list) if common_transform_list else None
 
     def __len__(self):
         return len(self.data_samples)
@@ -29,38 +33,55 @@ class HomographicDataset(BaseDataset):
 
     def __getitem__(self, index: int):
         sample = self.data_samples[index]
-        img = self.img_transform(sample.load_img())
-        points = torch.Tensor(sample.load_points()).permute(2, 0, 1) # (h, w, c) => (c h w)
-        c, h, w = img.shape
+        img = np.array(sample.load_img(as_gray=True))
+        if sample.points_file is None:
+            points, desc = self.point_detector(img, return_as_array=False)
+            points = points[:, :, np.newaxis]
+        else:
+            points = sample.load_points()
+        if self.crop_size is not None:
+            img, points = self._random_crop(img, points, self.crop_size)
+        if self.flip:
+            img, points = self._random_horizontal_flip(img, points)
+        h, w = img.shape
         homography = generate_random_homography(w, h)
-        warped_img, warped_points = self.make_homographic_sample(sample, homography)
-        warped_img = self.img_transform(warped_img)
+        warped_img, warped_points = self.make_homographic_sample(img, points, homography)
+
+        warped_img = self.img_transform(Image.fromarray(warped_img))
         warped_points = torch.Tensor(warped_points).permute(2, 0, 1) # (h, w, c) => (c h w)
+        
+        img = self.img_transform(Image.fromarray(img))
+        points = torch.Tensor(points).permute(2, 0, 1) # (h, w, c) => (c h w)
 
         correspondence_mask = homography.get_correspondence_mask()
 
-        if self.common_transform:
-            img, warped_img, points, warped_points = self.apply_common_transform(img, warped_img, points, warped_points)
-        return img, warped_img, self.rearrange_points_img(points), self.rearrange_points_img(warped_points), correspondence_mask
+        return img, self.rearrange_points_img(points), warped_img, self.rearrange_points_img(warped_points), torch.Tensor(correspondence_mask)
+    
+    def _random_crop(self, img: np.ndarray, points: np.ndarray, crop_size: int):
+        h, w = img.shape[:2]
+        x = np.random.randint(0, w - crop_size)
+        y = np.random.randint(0, h - crop_size)
+        img = img[y:y+crop_size, x:x+crop_size]
+        points = points[y:y+crop_size, x:x+crop_size]
+        return img.copy(), points.copy()
+    
+    def _random_horizontal_flip(self, img: np.ndarray, points: np.ndarray):
+        if np.random.rand() > 0.5:
+            img = np.flip(img, axis=1)
+            points = np.flip(points, axis=1)
+        return img.copy(), points.copy()
 
-    def make_homographic_sample(self, sample: DataSample, homography: TransformHomography):
-        img = np.array(sample.load_img())
-        points = sample.load_points()
+    def make_homographic_sample(self, img: np.ndarray, points: np.ndarray, homography: TransformHomography):
         warped_img = homography.apply(img)
         warped_points = homography.apply(points)
         if len(warped_points.shape) == 2:
             warped_points = warped_points[:, :, np.newaxis]
         return warped_img, warped_points
-    
-    def apply_common_transform(self, img: torch.Tensor, warp_img: torch.Tensor, points: torch.Tensor, warp_points: torch.Tensor):
-        img_channels = img.shape[0]
-        stacked = torch.vstack([img, warp_img])
-        stacked_transformed = self.common_transform(stacked)
-        return stacked_transformed[:img_channels], stacked_transformed[img_channels:], points, warp_points
 
     def rearrange_points_img(self, points: torch.Tensor):
         c, h, w = points.shape
         points = rearrange(points[0], "(hc ch1) (wc ch2) -> hc wc ch1 ch2", hc=h//8, wc=w//8, ch1=8, ch2=8)  # (1, h, w) => (h/8, w/8, 8, 8)
         points = rearrange(points, "h w ch1 ch2 -> h w (ch1 ch2)", ch1=8, ch2=8)  # (h/8, w/8, 8, 8) => (h/8, w/8, 64)
-        is_point_exist = (points.sum(dim=2) > 0).unsqueeze(-1)  # (h/8, w/8)
-        return torch.concat([points, is_point_exist], dim=2).permute(2, 0, 1)  # (h/8, w/8, 65) => (65, h/8, w/8)
+        is_point_not_exist = (points.sum(dim=2) == 0).unsqueeze(-1)  # (h/8, w/8)
+        points = torch.concat([points, is_point_not_exist], dim=2).permute(2, 0, 1)  # (h/8, w/8, 65) => (65, h/8, w/8)
+        return torch.argmax(points, dim=0, keepdim=False)  # (h/8, w/8)
--- superpanopoint/models/detector.py
+++ superpanopoint/models/detector.py
@@ -5,12 +5,14 @@ from PIL import Image
 from torchvision.transforms.v2 import Compose, Normalize, ToTensor
 
 from . import model_factory
-from .decoders.superpoint import postprocess_pointness
+from .decoders.superpoint import postprocess_descriptor, postprocess_pointness
 
 
-class PointDetector:
-    def __init__(self, cfg, weight_file) -> None:
+class Predictor:
+    def __init__(self, cfg, weight_file, device="cpu") -> None:
+        self.device = device
         self.net = model_factory(cfg)
+        self.net.to(self.device)
         self.net.load_state_dict(torch.load(weight_file)["state_dict"])
         self.net.eval()
         self.img_transform = Compose([
@@ -18,22 +20,28 @@ class PointDetector:
             Normalize(mean=[0.449], std=[0.226])
         ])
 
-    def __call__(self, img: np.ndarray, return_as_array: bool=False) -> np.ndarray:
-        img_tensor = self._preprocess(img)
+    def __call__(self, img: np.ndarray, return_as_array: bool=False) -> tuple[np.ndarray, np.ndarray]:
+        h,w = img.shape[:2]
+        img_tensor = self._preprocess(img).to(self.device)
         with torch.no_grad():
-            pred_pointness, _ = self.net(img_tensor)
+            pred_pointness, desc = self.net(img_tensor)
             pred_pointness = self._postprocess(pred_pointness)
+            desc = postprocess_descriptor(desc)[0]
 
         if return_as_array:
-            return self._to_points_array(pred_pointness)
-        
-        return pred_pointness
+            return self._to_points_array(pred_pointness, w, h)
+        if h != pred_pointness.shape[0] or w != pred_pointness.shape[1]:
+            pred_pointness = cv2.resize(pred_pointness, (w, h), interpolation=cv2.INTER_NEAREST)
+        return pred_pointness, desc
     
     def _preprocess(self, img: np.ndarray) -> torch.Tensor:
         if isinstance(img, Image.Image):
             img = np.array(img)
         if len(img.shape) == 3 and img.shape[2] == 3:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        h, w = img.shape
+        if h % 8 != 0 or w % 8 != 0:
+            img = cv2.resize(img, (w // 8 * 8, h // 8 * 8))
         img = self.img_transform(img)
         return img.unsqueeze(0)
     
@@ -41,6 +49,9 @@ class PointDetector:
         """input: (1, 65, H/8, W/8), output: (H, W)"""
         return postprocess_pointness(pred_pointness)[0]
     
-    def _to_points_array(self, img: np.ndarray) -> np.ndarray:
-        return np.array(np.where(img > 0)).T
-        
+    def _to_points_array(self, img: np.ndarray, orig_w, orig_h) -> np.ndarray:
+        h, w = img.shape[:2]
+        coords = np.array(np.where(img > 0)).T.astype(float)
+        coords[:, 0] *= orig_h / h
+        coords[:, 1] *= orig_w / w
+        return np.round(coords).astype(int)
