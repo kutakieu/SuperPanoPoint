9abeff61f80ece200c441a49898d8d390811a289
kutakieu <kutakieu22@gmail.com>
2023-11-29 22:34:02 +0900

    fix homographic training

--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -13,20 +13,19 @@ model = model_factory(cfg)
 
 def test_detector_loss():
     for batch in train_dataloader:
-        img, warped_img, points, warped_points, correspondence_mask = batch
+        img, points, warped_img, warped_points, correspondence_mask = batch
 
         pred_pointness, pred_desc = model(img)
         pred_warped_pointness, pred_warped_desc = model(warped_img)
-        print("pred_pointness.shape, points.shape", pred_pointness.shape, points.shape)
         loss = pointness_loss(pred_pointness, points)
         assert loss.shape == torch.Size([])
 
 def test_descriptor_loss():
     for batch in train_dataloader:
-        img, warped_img, points, warped_points, correspondence_mask = batch
+        img, points, warped_img, warped_points, correspondence_mask = batch
 
         pred_pointness, pred_desc = model(img)
         pred_warped_pointness, pred_warped_desc = model(warped_img)
-        print(pred_desc.shape, pred_warped_desc.shape)
         loss = descriptor_loss(pred_desc, pred_warped_desc, correspondence_mask)
         assert loss.shape == torch.Size([])
+
