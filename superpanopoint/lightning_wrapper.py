4b005f52e88c9f69c96fae116c30fa7db5483421
kutakieu <kutakieu22@gmail.com>
2023-11-30 23:48:35 +0900

    refactor training

--- superpanopoint/lightning_wrapper.py
+++ superpanopoint/lightning_wrapper.py
@@ -4,8 +4,8 @@ import numpy as np
 from lightning import pytorch as pl
 from torch import Tensor
 
-from superpanopoint.lossfn_optimizer import (descriptor_loss,
-                                             loss_function_factory,
+from superpanopoint.lossfn_optimizer import (descriptor_loss_fn,
+                                             make_pointness_loss_fn,
                                              optimizer_factory)
 from superpanopoint.models import model_factory
 from superpanopoint.utils.logger import get_logger
@@ -18,8 +18,8 @@ class LightningWrapper(pl.LightningModule):
         super().__init__()
         self.cfg = cfg
         self.net = model_factory(cfg)
-        self.lossfn_pointness = loss_function_factory("cross_entropy")
-        self.lossfn_descriptor = descriptor_loss
+        self.lossfn_pointness = make_pointness_loss_fn(cfg.training.get("pointness_loss_weight", 1000.0))
+        self.lossfn_descriptor = descriptor_loss_fn
         self.desc_loss_weight = cfg.training.get("desc_loss_weight", 250.0)
         self.optimizer_name = cfg.training.optimizer.name
         self.learning_rate = cfg.training.optimizer.learning_rate
@@ -31,7 +31,7 @@ class LightningWrapper(pl.LightningModule):
         return self.lossfn_pointness(pred, gt)
 
     def _clac_description_loss(self, desc: Tensor, warped_desc: Tensor, correspondence_mask: Tensor):
-        return descriptor_loss(desc, warped_desc, correspondence_mask)
+        return self.lossfn_descriptor(desc, warped_desc, correspondence_mask)
 
     # override
     def configure_optimizers(self):
@@ -40,21 +40,24 @@ class LightningWrapper(pl.LightningModule):
 
     # override
     def training_step(self, batch, batch_idx):
-        if self.cfg.dataset.type == "synthetic":
+        if self.cfg.model.name == "magicpoint":
             img, points = batch
-            pointness, desc = self.net(img)
-            loss_desc = loss_warped_pointness = 0.0
-        else:
+            pointness = self.net(img)
+            loss_total = self._clac_pointness_loss(pointness, points)
+            self.log("loss_pointness_train_step", loss_total, sync_dist=True)
+        elif self.cfg.model.name == "superpoint":
             img, points, warped_img, warped_points, correspondence_mask = batch
             pointness, desc = self.net(img)
             warped_pointness, warped_desc = self.net(warped_img)
             loss_warped_pointness = self._clac_pointness_loss(warped_pointness, warped_points)
             loss_desc = self._clac_description_loss(desc, warped_desc, correspondence_mask)
             self.log("loss_desc_train_step", loss_desc, sync_dist=True)
-        loss_pointness = self._clac_pointness_loss(pointness, points) + loss_warped_pointness
-        self.log("loss_pointness_train_step", loss_pointness, sync_dist=True)
+            loss_pointness = self._clac_pointness_loss(pointness, points) + loss_warped_pointness
+            self.log("loss_pointness_train_step", loss_pointness, sync_dist=True)
+            loss_total = loss_pointness + self.desc_loss_weight * loss_desc
+        else:
+            raise NotImplementedError
         
-        loss_total = loss_pointness + self.desc_loss_weight * loss_desc
         self.training_step_outputs.append({"loss": loss_total.detach().cpu().numpy()})
         return loss_total
 
@@ -66,24 +69,22 @@ class LightningWrapper(pl.LightningModule):
 
     # override
     def validation_step(self, batch, batch_idx):
-        if self.cfg.dataset.type == "synthetic":
+        if self.cfg.model.name == "magicpoint":
             img, points = batch
-            pointness, desc = self.net(img)
-            loss_desc = loss_warped_pointness = 0.0
+            pointness = self.net(img)
+            loss_total = self._clac_pointness_loss(pointness, points)
         else:
             img, points, warped_img, warped_points, correspondence_mask = batch
             pointness, desc = self.net(img)
             warped_pointness, warped_desc = self.net(warped_img)
             loss_warped_pointness = self._clac_pointness_loss(warped_pointness, warped_points)
             loss_desc = self._clac_description_loss(desc, warped_desc, correspondence_mask)
-            self.log("loss_desc_train_step", loss_desc, sync_dist=True)
-        loss_pointness = self._clac_pointness_loss(pointness, points) + loss_warped_pointness
-        self.log("loss_pointness_train_step", loss_pointness, sync_dist=True)
+            loss_pointness = self._clac_pointness_loss(pointness, points) + loss_warped_pointness
+            loss_total = loss_pointness + self.desc_loss_weight * loss_desc
 
         # metrics = self.calc_metrics()
-        loss_total = loss_pointness + self.desc_loss_weight * loss_desc
         self.validation_step_outputs.append({"loss": loss_total.detach().cpu().numpy(), "metrics": 0})
-        return loss_pointness
+        return loss_total
 
     # override
     def on_validation_epoch_end(self) -> None:
@@ -94,9 +95,9 @@ class LightningWrapper(pl.LightningModule):
 
     # override
     def test_step(self, batch, batch_idx):
-        if self.cfg.dataset.type == "synthetic":
+        if self.cfg.model.name == "magicpoint":
             img, points = batch
-            pointness, desc = self.net(img)
+            pointness = self.net(img)
             loss_desc = loss_warped_pointness = 0.0
         else:
             img, points, warped_img, warped_points, correspondence_mask = batch
@@ -104,9 +105,7 @@ class LightningWrapper(pl.LightningModule):
             warped_pointness, warped_desc = self.net(warped_img)
             loss_warped_pointness = self._clac_pointness_loss(warped_pointness, warped_points)
             loss_desc = self._clac_description_loss(desc, warped_desc, correspondence_mask)
-            self.log("loss_desc_train_step", loss_desc, sync_dist=True)
         loss_pointness = self._clac_pointness_loss(pointness, points) + loss_warped_pointness
-        self.log("loss_pointness_train_step", loss_pointness, sync_dist=True)
 
         # metrics = self.calc_metrics()
         loss_total = loss_pointness + self.desc_loss_weight * loss_desc
