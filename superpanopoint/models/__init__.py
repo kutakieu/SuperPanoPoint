e41d111f3ae93176de4ed2da3ff58814d46b3f25
kutakieu <kutakieu22@gmail.com>
2023-12-09 00:28:26 +0900

    make input color

--- superpanopoint/models/__init__.py
+++ superpanopoint/models/__init__.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from omegaconf import DictConfig
 from torch import nn
 
 from .decoders.superpoint import (PointDescriptor, PointDetector,
@@ -10,25 +11,25 @@ from .encoders.vgg import VGG
 def model_factory(cfg):
     if cfg.model.name in ["magicpoint", "superpoint"]:
         encoder = _encoder_factory(cfg.model.encoder)
-        decoder = _decoder_factory(cfg.model.decoder, cfg.model.get("description_dim"))
+        decoder = _decoder_factory(cfg.model.decoder)
     else:
         raise NotImplementedError
     return nn.Sequential(encoder, decoder)
 
-def _encoder_factory(encoder_name: str):
-    if encoder_name == "vgg":
-        return VGG()
+def _encoder_factory(encoder_cfg: DictConfig):
+    if encoder_cfg.name == "vgg":
+        return VGG(num_color_chs=encoder_cfg.get("color_channels", 3))
     else:
         raise NotImplementedError
 
     
-def _decoder_factory(decoder_name: str, description_dim: Optional[int]):
-    if decoder_name == "superpoint":
+def _decoder_factory(decoder_cfg: DictConfig):
+    if decoder_cfg.name == "superpoint":
         return SuperPointDecoder(
             detector=PointDetector(in_channels=512, out_channels=65),
-            descriptor=PointDescriptor(in_channels=512, out_channels=description_dim),
+            descriptor=PointDescriptor(in_channels=512, out_channels=decoder_cfg.get("description_dim", 256)),
         )
-    elif decoder_name == "magicpoint":
+    elif decoder_cfg.name == "magicpoint":
         return PointDetector(in_channels=512, out_channels=65)
     else:
         raise NotImplementedError
