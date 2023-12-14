7df281cb587e03f029a5f4acd52c44b4b3a06807
kutakieu <kutakieu22@gmail.com>
2023-12-14 12:00:58 +0900

    minor fixes

--- superpanopoint/__init__.py
+++ superpanopoint/__init__.py
@@ -1,18 +1,10 @@
-import os
 from pathlib import Path
-
-DATA_DIR = Path(os.environ.get("DATA_DIR", "data"))
 
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    """
-    環境変数を読み取り保持するクラス
-    アルファベットの大文字小文字問わず、メンバー変数名と同じ環境変数名が存在する場合はその値が読み取られる
-    """
-
     config_dir: str = Field(default="config")
     config_name: str = Field(default="config.yaml")
 
@@ -26,5 +18,4 @@ class Settings(BaseSettings):
 
     wandb_api_key: str = Field(default="")
 
-    # 主にローカルでの開発時に.envファイルを参照するように設定
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
